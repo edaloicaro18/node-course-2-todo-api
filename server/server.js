2210cbbf9bbf76d548fc3ad2dbd850cc83ee628a
Danny Miller <edaloicaro18@gmail.com>
2017-08-08 11:38:17 -0500

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -95,7 +95,7 @@ app.patch('/todos/:id', (req, res) => {
 app.post('/users/', (req, res) => {
     const body = _.pick(req.body, ['email', 'password']);
     const user = new User(body);
-    
+
     user.save()
         .then(() => user.generateAuthToken())
         .then(token => res.header('x-auth', token).send(user))
@@ -105,6 +105,18 @@ app.post('/users/', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+    const body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then(user => {
+        return user.generateAuthToken().then(token => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch(e => {
+        res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
