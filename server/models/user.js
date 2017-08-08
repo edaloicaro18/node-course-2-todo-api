7bca6d016d625171d0e46eb050385fe4e338184a
Danny Miller <edaloicaro18@gmail.com>
2017-08-08 17:20:33 -0500

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -51,6 +51,16 @@ UserSchema.methods.generateAuthToken = function () {
     });
 };
 
+UserSchema.methods.removeToken = function (token) {
+    const user = this;
+
+    return user.update({
+        $pull: {
+            tokens: { token }
+        }
+    });
+};
+
 UserSchema.statics.findByToken = function (token) {
     const User = this;
     let decoded;
@@ -76,7 +86,7 @@ UserSchema.statics.findByCredentials = function (email, password) {
 
         return new Promise((resolve, reject) => {
 
-            bcrypt.compare(password, user.password, (err, res) => {               
+            bcrypt.compare(password, user.password, (err, res) => {
                 if (!res) {
                     reject();
                 }
